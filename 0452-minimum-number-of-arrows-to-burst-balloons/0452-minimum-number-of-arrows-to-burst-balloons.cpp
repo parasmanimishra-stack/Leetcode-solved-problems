dd8a7e53f098749b377f1d377f94d18c15a6ae05
parasmanimishra-stack <parasmanimishra69@gmail.com>
2026-08-18 18:57:27 +0530

    Time: 43 ms (79.19%), Space: 93.8 MB (82.5%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,13 +1,11 @@
-bool cmp(vector<int> a, vector<int> b){
-    return a[0] < b[0];
-}
+
 
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& intervals) {
          
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end(), cmp);
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){return a[0] < b[0];});
         int Lastend = intervals[0][1];
            int ans = 0;
         for(int i=1; i<n; i++){
