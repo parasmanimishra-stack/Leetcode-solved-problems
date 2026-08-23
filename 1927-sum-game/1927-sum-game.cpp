d74befeb9c8df0cf5ae3413f8921a2c1eca9ffa7
parasmanimishra-stack <parasmanimishra69@gmail.com>
2026-08-23 15:03:03 +0530

    Time: 4 ms (47.55%), Space: 14 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n =  num.size();
+        int diff = 0, q = 0;
+        for(int i=0; i<n/2; i++){
+           if(num[i] == '?') q++;
+           else {
+            diff += (num[i] - '0');
+           }
+        }
+        for(int i=n/2; i < n; i++){
+               if(num[i] == '?') q--;
+           else {
+            diff -= (num[i] - '0');
+           }
+            
+        }
+        if(q%2 != 0) return 1;
+        else return ( diff + (q*9)/2) != 0;
+
+        
+    }
+};
